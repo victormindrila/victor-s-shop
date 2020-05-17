60c3192d94c152981cc0dd224f11ddba9da627b2
Victor Mindrila <victormindrila@gmail.com>
2020-05-17 11:06:04 +0300

    feat(view): adds HomeCategory

---
+++ view/src/components/HomeCategory/HomeCategory.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const HomeCategory = (props) => {
+	const { route, name, description, image } = props;
+
+	return (
+		<div className='col-12 col-md-6 mb-3'>
+			<Link to={`/category/${route}`}>
+				<div className='w-100'>
+					<img src={image} alt={name} className='w-100' />
+				</div>
+				<h2 className='h4 my-1'>
+					<strong>{name}:</strong>
+				</h2>
+				<p className='m-0'>{description}</p>
+			</Link>
+		</div>
+	);
+};
+
+export default HomeCategory;
