70139ba5a2ec5107dcd74c6fdb03634d770d474d
Victor Mindrila <victormindrila@gmail.com>
2020-05-17 12:41:50 +0300

    feat(view): adds Home page

--- view/src/pages/Home/Home.jsx
+++ view/src/pages/Home/Home.jsx
@@ -1,16 +1,68 @@
 import React from 'react';
-
-import './Home.css';
-import Slider from '../../components/Slider/Slider';
-
+import { Link } from 'react-router-dom';
 import Layout from '../../components/Layout/Layout';
+import HomeCategory from '../../components/HomeCategory/HomeCategory';
 
-function Home() {
-	return (
-		<Layout>
-			<h1> hello </h1>
-		</Layout>
-	);
+//helpers
+import axios from 'axios';
+
+class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			categories: [],
+			loading: false,
+			error: ''
+		};
+	}
+
+	componentDidMount() {
+		this.fetchCategories();
+	}
+
+	fetchCategories() {
+		this.setState({
+			loading: true
+		});
+		axios
+			.get('/categories')
+			.then((response) => {
+				this.setState({
+					categories: response.data,
+					loading: false
+				});
+			})
+			.catch((error) => {
+				this.setState({
+					error: error.response.data.error
+				});
+			});
+	}
+
+	render() {
+		console.log(this.state.categories);
+		return (
+			<Layout>
+				<div className='container-fluid container-min-max-width'>
+					<div className='row'>
+						{this.state.categories.map((category) => {
+							const { id, name, description, imageUrl } = category;
+							return <HomeCategory key={id} route={category} name={name} description={description} image={imageUrl} />;
+						})}
+					</div>
+					<div>
+						<hr />
+						<div className='d-flex justify-content-center'>
+							<Link to='/products'>
+								<button className='btn btn-outline-dark my-3'>View all products</button>
+							</Link>
+						</div>
+						<hr />
+					</div>
+				</div>
+			</Layout>
+		);
+	}
 }
 
 export default Home;
