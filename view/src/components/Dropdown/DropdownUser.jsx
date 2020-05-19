03bc504f966e1be037d6a77d3b141af69f9c2214
Victor Mindrila <victormindrila@gmail.com>
2020-05-19 21:45:22 +0300

    fix(view): updates user dropdown

--- view/src/components/Dropdown/DropdownUser.jsx
+++ view/src/components/Dropdown/DropdownUser.jsx
@@ -2,33 +2,39 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 // redux
 import { connect } from 'react-redux';
-import { logoutUser } from '../../store/actions/user';
+import { logoutUser, getUserData } from '../../store/actions/user';
 
-function DropdownUser({ isActive, user, logoutUser }) {
-	return (
-		<div className={`dropdown-menu ${isActive ? 'show' : ''}`}>
-			{user ? <p className='ml-4'>Salut, {user.firstName || user.displayName}!</p> : null}
+class DropdownUser extends React.Component {
+	componentDidMount() {
+		if (!this.props.user) this.props.getUserData();
+	}
+	render() {
+		const { isActive, user, logoutUser } = this.props;
+		return (
+			<div className={`dropdown-menu ${isActive ? 'show' : ''}`}>
+				{user ? <p className='ml-4'>Salut, {user.firstName || user.displayName}!</p> : null}
 
-			{user ? (
-				<div>
-					<div class='dropdown-divider' />
-					<button className='dropdown-item' onClick={() => logoutUser()}>
-						Delogare
-					</button>
-				</div>
-			) : (
-				<div>
-					<Link to='/login' className='dropdown-item'>
-						Logare
-					</Link>
-					<div class='dropdown-divider' />
-					<Link to='/signup' className='dropdown-item'>
-						Inregistrate
-					</Link>
-				</div>
-			)}
-		</div>
-	);
+				{user ? (
+					<div>
+						<div class='dropdown-divider' />
+						<button className='dropdown-item' onClick={() => logoutUser()}>
+							Delogare
+						</button>
+					</div>
+				) : (
+					<div>
+						<Link to='/login' className='dropdown-item'>
+							Logare
+						</Link>
+						<div class='dropdown-divider' />
+						<Link to='/signup' className='dropdown-item'>
+							Inregistrate
+						</Link>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
 
 function mapStateToProps(state) {
@@ -41,6 +47,9 @@ function mapDispatchToProps(dispatch) {
 	return {
 		logoutUser: () => {
 			dispatch(logoutUser());
+		},
+		getUserData: () => {
+			dispatch(getUserData());
 		}
 	};
 }
