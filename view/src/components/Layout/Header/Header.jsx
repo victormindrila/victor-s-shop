cb240311293f77915e3f94ada785c8e7dc2987c6
Victor Mindrila <victormindrila@gmail.com>
2020-05-24 10:42:04 +0300

    fix(view): converts header to function component

--- view/src/components/Layout/Header/Header.jsx
+++ view/src/components/Layout/Header/Header.jsx
@@ -15,45 +15,35 @@ import { ReactComponent as Favorite } from '../../../assets/icons/favorite.svg';
 import DropdownUser from '../../Dropdown/DropdownUser';
 import DropdownSearch from '../../Dropdown/DropdownSearch';
 
-class Header extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			displayUserDropdown: false
-		};
-	}
+function Header({ numberOfProducts, user }) {
+	return (
+		<header className='border-bottom mb-3 '>
+			<div className='container-fluid container-min-max-width d-flex justify-content-between align-items-center'>
+				<Link to='/' className='my-3'>
+					<img src={logo} alt={`Victor's shop`} className='logo' />
+				</Link>
 
-	render() {
-		const { numberOfProducts, user } = this.props;
-		return (
-			<header className='border-bottom mb-3 '>
-				<div className='container-fluid container-min-max-width d-flex justify-content-between align-items-center'>
-					<Link to='/' className='my-3'>
-						<img src={logo} alt={`Victor's shop`} className='logo' />
-					</Link>
+				<DropdownSearch />
+				<DropdownUser />
 
-					<DropdownSearch />
-					<DropdownUser />
+				<div>
+					<div className='d-flex justify-content-end'>
+						<div className='d-flex align-items-center'>
+							<Link to='/products/?category=favorites' className='d-flex'>
+								<Favorite className='mr-2' />
+								<p className='products-number ml-1 mb-0'>{user.data && user.data.favorites.length}</p>
+							</Link>
 
-					<div>
-						<div className='d-flex justify-content-end'>
-							<div className='d-flex align-items-center'>
-								<Link to='/products/?category=favorites' className='d-flex'>
-									<Favorite className='mr-2' />
-									<p className='products-number ml-1 mb-0'>{user.data && user.data.favorites.length}</p>
-								</Link>
-
-								<Link to='/cart' className='d-flex'>
-									<ShoppingCart className='ml-2' />
-									<p className='products-number ml-1 mb-0'>{numberOfProducts}</p>
-								</Link>
-							</div>
+							<Link to='/cart' className='d-flex'>
+								<ShoppingCart className='ml-2' />
+								<p className='products-number ml-1 mb-0'>{numberOfProducts}</p>
+							</Link>
 						</div>
 					</div>
 				</div>
-			</header>
-		);
-	}
+			</div>
+		</header>
+	);
 }
 
 function mapStateToProps(state) {
