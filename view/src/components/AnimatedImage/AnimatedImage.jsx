7953d621de8eff8bb141ef0993a9570579c00af0
victor_mindrila <victormindrila@gmail.com>
2020-09-07 22:34:19 +0300

    adds progressive image loading with intersection observer

--- view/src/components/AnimatedImage/AnimatedImage.jsx
+++ view/src/components/AnimatedImage/AnimatedImage.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
+import useIntersectionObserver from '../../utils/useIntersectionObserver';
 
 function AnimatedImage({ imageUrl, alt, ...rest }) {
+	const ref = React.useRef();
+	const [ isVisible, setIsVisible ] = React.useState(false);
+	useIntersectionObserver({
+		target: ref,
+		onIntersect: ([ { isIntersecting } ], observerElement) => {
+			if (isIntersecting) {
+				setIsVisible(true);
+				observerElement.unobserve(ref.current);
+			}
+		}
+	});
+
 	return (
-		<div>
-			<Fade>
-				<img src={imageUrl} alt={alt} {...rest} />
-			</Fade>
+		<div ref={ref}>
+			{isVisible && (
+				<Fade>
+					<img src={imageUrl} alt={alt} {...rest} />
+				</Fade>
+			)}
 		</div>
 	);
 }
---
+++ view/src/utils/useIntersectionObserver.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+const useIntersectionObserver = ({ target, onIntersect, threshold = 0.1, rootMargin = '0px' }) => {
+	useEffect(() => {
+		const observer = new IntersectionObserver(onIntersect, {
+			rootMargin,
+			threshold
+		});
+		const current = target.current;
+		observer.observe(current);
+		return () => {
+			observer.unobserve(current);
+		};
+	});
+};
+export default useIntersectionObserver;
